ad2f57bb7eaccec6506b9c36c0a1b2bc9918b346
Lawrence <larry12193@gmail.com>
2019-11-30 02:00:56 +0000

    ROS based joystick control of robot

--- teensy_firmware/teensy_firmware.ino
+++ teensy_firmware/teensy_firmware.ino
@@ -6,6 +6,47 @@
 #define DELAY 1000
 #define STEP_COUNT 1000
 
+#define AXES_LEFT_JOY_LR  0
+#define AXES_LEFT_JOY_UD  1
+#define AXES_LEFT_LT      2
+#define AXES_RIGHT_JOY_LR 3
+#define AXES_RIGHT_JPY_UD 4
+#define AXES_RIGHT_RT     5
+#define AXES_DIR_PAD_LR   6
+#define AXES_DIR_PAD_UD   7
+
+#define BUTTON_A         0
+#define BUTTON_B         1
+#define BUTTON_X         2
+#define BUTTON_Y         3
+#define BUTTON_LB        4
+#define BUTTON_RB        5
+#define BUTTON_BACK      6
+#define BUTTON_START     7
+#define BUTTON_XBOX      8
+#define BUTTON_LEFT_JOY  9
+#define BUTTON_RIGHT_JOY 10
+
+#define MAGNITUDE_DEADZONE 0.1
+
+#include <ros.h>
+#include <sensor_msgs/Joy.h>
+
+volatile double left_x = 0;
+volatile double left_y = 0;
+
+void joy_Callback( const sensor_msgs::Joy& joy ) {
+
+  left_x = joy.axes[AXES_LEFT_JOY_LR];
+  left_y = joy.axes[AXES_LEFT_JOY_UD];
+
+  double magnitude = sqrt(pow(left_x,2) + pow(left_y,2));
+  if( magnitude < MAGNITUDE_DEADZONE ) {
+    left_x = 0;
+    left_y = 0;
+  }
+}
+
 int nstep = 0;
 int dir_multi = 1;
 char serIn;
@@ -28,30 +69,18 @@ void setup() {
 
 void loop() {
 
-
   move = false;
 
-  if( Serial.available() > 0 ) {
-    serIn = Serial.read();
-    
-    if( serIn == 'W' ) {
-      digitalWrite(R_DIR_PIN, HIGH);
-      digitalWrite(L_DIR_PIN, HIGH);
-      move = true;
-    } else if( serIn == 'S' ) {
-      digitalWrite(R_DIR_PIN, LOW);
-      digitalWrite(L_DIR_PIN, LOW);
-      move = true;
-    } else if( serIn == 'A' ) {
-      digitalWrite(R_DIR_PIN, HIGH);
-      digitalWrite(L_DIR_PIN, LOW);
-      move = true;
-    } else if( serIn == 'D' ) {
-      digitalWrite(R_DIR_PIN, LOW);
-      digitalWrite(L_DIR_PIN, HIGH);
-      move = true;
-    }
+  if( left_x > 0 ) {
+    digitalWrite(R_DIR_PIN, HIGH);
+    digitalWrite(L_DIR_PIN, HIGH);
+    move = true;
+  } else if( left_x < 0 ) {
+    digitalWrite(R_DIR_PIN, LOW);
+    digitalWrite(L_DIR_PIN, LOW);
+    move = true;
   }
+
   if ( move ) {
     for(int i = 0; i < 50; i++ ){
       digitalWrite(R_STEP_PIN,HIGH);
